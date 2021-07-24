98ecf5ee53c2f3552a136a9ed7f055c152c41d5f
Honey <yurjune@naver.com>
2021-07-25 01:58:29 +0900

    ch6 update

--- ch6/6.5.2_nunjucks/app.js
+++ ch6/6.5.2_nunjucks/app.js
@@ -2,45 +2,31 @@ const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const dotenv = require('dotenv'); // 환경변수나 비밀키 관리
+const dotenv = require('dotenv');
 const path = require('path');
 const nunjucks = require('nunjucks');
 
-dotenv.config();  // 이 코드는 process.env를 쓰는 패키지보다 위에 위치해야 한다
+dotenv.config();
 const indexRouter = require('./routes');
 const userRouter = require('./routes/user');
 
 const app = express();
 app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'html');
+nunjucks.configure('views', {
+  express: app,
+  watch: true,
+});
 
-
-// 써드파티 미들웨어
-// 1 morgan
 app.use(morgan('dev'));
-
-// 2 static
-// app.use('요청 경로', express.static('실제 경로'))
-// 파일을 찾으면 next() 실행하지 않음
 app.use('/', express.static(path.join(__dirname, 'public')));
-
-// 3 body-parser
-// for parsing application/json
-// 클라이언트가 보낸 json데이터를 파싱해서 req.body에 넣음
 app.use(express.json());
-
-// for parsing application/x-www-form-urlencoded
-// extended: true: qs모듈, false: queryString모듈
 app.use(express.urlencoded({ extended: false }));
-
-// 4 cookie-parser
-// res.cookie로 set-cookie, req.cookies로 접근
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
-// 5 session
 app.use(session({
   resave: false,
   saveUninitialized: false,
-  secret: process.env.COOKIE_SECRET,  // 환경변수에 비밀 키를 숨겨놓음
+  secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
     secure: false,
@@ -48,19 +34,22 @@ app.use(session({
   name: 'session-cookie',
 }));
 
-
-// 라우터 분리
 app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 
+// 404처리도 에러처리미들웨어에서 하기
 app.use((req, res, next) => {
-  res.status(404).send('Not Found');
+  const error =  new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status = 404;
+  next(error);
 });
 
 app.use((err, req, res, next) => {
-  console.error(err);
-  res.status(500).send(err.message);
+  res.locals.message = err.message;
+  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
 });
 
 app.listen(app.get('port'), () => {
