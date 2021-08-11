8239f31b262313b86484f7f4f91d607e0b9ff126
Honey <yurjune@naver.com>
2021-08-12 04:28:37 +0900

    update url

--- ch3/3.5_module/url.js
+++ ch3/3.5_module/url.js
@@ -1,14 +1,13 @@
 // url 모듈: 인터넷 주소를 쉽게 조작하도록 도와주는 모듈
 const url = require('url');
+const _url = 'http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor'
 
 // 1 WHATWG 방식
 const { URL } = url;
-const myURL = new URL('http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor');
+const myURL = new URL(_url);  // 구문분석
 console.log('new URL():', myURL);
-console.log('url.format():', url.format(myURL));
 console.log('------------------------------');
 
 // 2 기존 노드 방식
-const parsedUrl = url.parse('http://www.gilbut.co.kr/book/bookList.aspx?sercate1=001001000#anchor');
+const parsedUrl = url.parse(_url);  // 구문분석
 console.log('url.parse():', parsedUrl);
-console.log('url.format():', url.format(parsedUrl));
